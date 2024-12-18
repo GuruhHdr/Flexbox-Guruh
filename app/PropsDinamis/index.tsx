92a3b0e7955360969ed9abdf0cbf171ef86cd9f4
GuruhHdr <hendryan.g017@gmail.com>
2024-12-18 13:10:30 +0700

    Props_Dinamis_State

--- app/PropsDinamis/index.tsx
+++ app/PropsDinamis/index.tsx
@@ -1,13 +1,123 @@
-import { View, Text, Image } from 'react-native';
+import { StyleSheet, View, Text, Image, ScrollView } from 'react-native';
 import React, { Component } from 'react';
 
+const Story = props => {
+  return (
+    <View style={{alignItems: 'center', marginRight: 20}}>
+      <Image 
+      source={{
+          uri: props.gambar,
+        }}
+        style={{width: 70, height: 70, borderRadius: 70 / 2}}
+      />
+      <Text style={{maxWidth: 50, textAlign: 'center'}}>{props.judul}</Text>
 
-const PropsDinamis =  () => {
+    </View>
+  )
+}
+
+const PropsDinamis = () => {
   return (
     <View>
       <Text>Materi PropsDinamis Boss</Text>
-      <Image source={{}}/>
+      <ScrollView horizontal>
+        <View style={{flexDirection: 'row'}}>
+          <Story 
+            judul="Dogs Gemoy Sekali" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Pugglesie.jpg/800px-Pugglesie.jpg"/>
+          <Story 
+            judul="Kucing" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg/800px-Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg"/>
+          <Story 
+            judul="Kuda" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Friesian_Stallion.jpg/457px-Friesian_Stallion.jpg"/>
+          <Story 
+            judul="Gajah" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/800px-African_Bush_Elephant.jpg"/>
+                  <Story 
+            judul="Dogs" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Pugglesie.jpg/800px-Pugglesie.jpg"/>
+          <Story 
+            judul="Kucing" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg/800px-Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg"/>
+          <Story 
+            judul="Kuda" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Friesian_Stallion.jpg/457px-Friesian_Stallion.jpg"/>
+          <Story 
+            judul="Gajah" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/800px-African_Bush_Elephant.jpg"/>
+                  <Story 
+            judul="Dogs" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Pugglesie.jpg/800px-Pugglesie.jpg"/>
+          <Story 
+            judul="Kucing" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg/800px-Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg"/>
+          <Story 
+            judul="Kuda" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Friesian_Stallion.jpg/457px-Friesian_Stallion.jpg"/>
+          <Story 
+            judul="Gajah" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/800px-African_Bush_Elephant.jpg"/>          <Story 
+            judul="Dogs" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Pugglesie.jpg/800px-Pugglesie.jpg"/>
+          <Story 
+            judul="Kucing" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg/800px-Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg"/>
+          <Story 
+            judul="Kuda" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Friesian_Stallion.jpg/457px-Friesian_Stallion.jpg"/>
+          <Story 
+            judul="Gajah" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/800px-African_Bush_Elephant.jpg"/>          <Story 
+            judul="Dogs" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Pugglesie.jpg/800px-Pugglesie.jpg"/>
+          <Story 
+            judul="Kucing" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg/800px-Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg"/>
+          <Story 
+            judul="Kuda" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Friesian_Stallion.jpg/457px-Friesian_Stallion.jpg"/>
+          <Story 
+            judul="Gajah" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/800px-African_Bush_Elephant.jpg"/>          <Story 
+            judul="Dogs" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Pugglesie.jpg/800px-Pugglesie.jpg"/>
+          <Story 
+            judul="Kucing" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg/800px-Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg"/>
+          <Story 
+            judul="Kuda" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Friesian_Stallion.jpg/457px-Friesian_Stallion.jpg"/>
+          <Story 
+            judul="Gajah" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/800px-African_Bush_Elephant.jpg"/>          <Story 
+            judul="Dogs" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Pugglesie.jpg/800px-Pugglesie.jpg"/>
+          <Story 
+            judul="Kucing" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg/800px-Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg"/>
+          <Story 
+            judul="Kuda" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Friesian_Stallion.jpg/457px-Friesian_Stallion.jpg"/>
+          <Story 
+            judul="Gajah" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/800px-African_Bush_Elephant.jpg"/>          <Story 
+            judul="Dogs" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Pugglesie.jpg/800px-Pugglesie.jpg"/>
+          <Story 
+            judul="Kucing" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg/800px-Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg"/>
+          <Story 
+            judul="Kuda" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Friesian_Stallion.jpg/457px-Friesian_Stallion.jpg"/>
+          <Story 
+            judul="Gajah" 
+            gambar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/African_Bush_Elephant.jpg/800px-African_Bush_Elephant.jpg"/>
+        
+        </View>  
+            
+      </ScrollView>
     </View>
   );
 };
-export default PropsDinamis;
+export default PropsDinamis;  
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
-import { View } from "react-native";
+import { StyleSheet, View, Text, Image} from "react-native";
 import Flexbox from "./Flexbox";
 import PropsDinamis from "./PropsDinamis";
+import StateDinamis from "./StateDinamis";
 
 export default function Index() {
   return (
     <View>
       <Flexbox />
       <PropsDinamis />
+      <StateDinamis />
     </View>
   );
 };
